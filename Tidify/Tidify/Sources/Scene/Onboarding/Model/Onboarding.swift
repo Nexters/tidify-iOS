2d4b38d874a1db2670aaa0a37d225a1e9a662f09
JungsuYeo <duwjdtn@ajou.ac.kr>
2021-08-29 23:10:50 +0900

    온보딩 구현 완료

---
+++ Tidify/Tidify/Sources/Scene/Onboarding/Model/Onboarding.swift
@@ -0,0 +1,16 @@
+//
+//  Onboarding.swift
+//  Tidify
+//
+//  Created by 여정수 on 2021/08/29.
+//
+
+import Foundation
+import UIKit
+
+struct Onboarding {
+    let title: String
+    let description: String
+    let image: UIImage
+    let buttonTitle: String
+}
