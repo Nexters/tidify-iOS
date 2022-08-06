58dfb87f20a17ebde3913b5fa1da6593633235f9
Iannealer <duwjdtn@ajou.ac.kr>
2022-08-06 17:18:49 +0900

    Feat: Coordinator 프로토콜 정의

---
+++ Tidify/Targets/TidifyPresentation/Sources/Base/Coordinator.swift
@@ -0,0 +1,28 @@
+//
+//  Coordinator.swift
+//  TidifyPresentation
+//
+//  Created by Ian on 2022/08/06.
+//  Copyright © 2022 Tidify. All rights reserved.
+//
+
+import UIKit
+
+/// Protocol that all coordinator must be conform
+public protocol Coordinator: AnyObject {
+
+  // MARK: - Properties
+  var childCoordinators: [Coordinator] { get set }
+  var navigationController: UINavigationController { get set }
+
+  // MARK: - Methods
+  func start()
+  func addChild(_ child: Coordinator)
+}
+
+// MARK: - Default Implementation
+extension Coordinator {
+  func addChild(_ child: Coordinator) {
+    childCoordinators.append(child)
+  }
+}
