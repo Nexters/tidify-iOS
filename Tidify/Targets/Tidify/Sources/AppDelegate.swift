b7ee79e9bb698126cc0402a9ddaea84a4a8f831f
Jeongsu Yeo <duwjdtn1@gmail.com>
2022-08-06 16:03:27 +0900

    Revert "Feat: 초기 셋팅"

--- Tidify/Targets/Tidify/Sources/AppDelegate.swift
+++ Tidify/Targets/Tidify/Sources/AppDelegate.swift
@@ -1,24 +1,21 @@
-import TidifyPresentation
 import UIKit
-
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window: UIWindow?
-  private var mainCoordinator: MainCoordinator?
+    var window: UIWindow?
 
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-  ) -> Bool {
-    let window = UIWindow(frame: UIScreen.main.bounds)
-    self.window = window
-    mainCoordinator = DefaultMainCoordiator(window: window)
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .white
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
 
-    mainCoordinator?.start()
-
-    return true
-  }
+        return true
+    }
 
 }
