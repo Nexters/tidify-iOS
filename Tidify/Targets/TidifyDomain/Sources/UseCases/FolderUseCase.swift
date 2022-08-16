ebdb22596fea6727881e1126f80b3ace22dd4339
Hansangjin98 <onetable98@naver.com>
2022-08-16 11:50:15 +0900

    Feat: FolderTab - UseCase 구현

---
+++ Tidify/Targets/TidifyDomain/Sources/UseCases/FolderUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  FolderUserCase.swift
+//  TidifyDomain
+//
+//  Created by 한상진 on 2022/08/16.
+//  Copyright © 2022 Tidify. All rights reserved.
+//
+
+import RxSwift
+
+public protocol FolderUseCase {
+  var folderRepository: FolderRepository { get set }
+
+  func fetchFolders() -> Observable<[Folder]?>
+}
+
+public final class DefaultFolderUseCase: FolderUseCase {
+
+  // MARK: - Properties
+  public var folderRepository: FolderRepository
+
+  // MARK: - Initializer
+  public init(repository: FolderRepository) {
+    self.folderRepository = repository
+  }
+
+  // MARK: - Methods
+  public func fetchFolders() -> Observable<[Folder]?> {
+    return folderRepository.fetchFolders()
+  }
+}
