85fed75151b711b798309ce924a990f766f7a282
Iannealer <duwjdtn@ajou.ac.kr>
2022-08-16 21:10:48 +0900

    Chore: UserSessionDTO 적용 및 import 순서 수정

---
+++ Tidify/Targets/TidifyData/Sources/Responses/UserSessionDTO.swift
@@ -0,0 +1,23 @@
+//
+//  UserSessionDTO.swift
+//  TidifyData
+//
+//  Created by Ian on 2022/08/16.
+//  Copyright © 2022 Tidify. All rights reserved.
+//
+
+import TidifyDomain
+
+struct UserSessionDTO: Decodable {
+  let authorization: String
+
+  enum CodingKeys: String, CodingKey {
+    case authorization = "Authorization"
+  }
+}
+
+extension UserSessionDTO {
+  func toDomain() -> UserSession {
+    .init(authorization: authorization)
+  }
+}
