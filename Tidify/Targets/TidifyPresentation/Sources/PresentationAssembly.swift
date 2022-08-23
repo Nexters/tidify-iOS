9cd58b1f5e0735c925227e5a1d5789148eb54fef
Iannealer <duwjdtn@ajou.ac.kr>
2022-08-23 20:18:47 +0900

    Chore: PresentationAssembly - HomeCoordinator 추가

--- Tidify/Targets/TidifyPresentation/Sources/PresentationAssembly.swift
+++ Tidify/Targets/TidifyPresentation/Sources/PresentationAssembly.swift
@@ -31,5 +31,9 @@ public struct PresentationAssembly: Assemblable {
     container.register(type: TabBarCoordinator.self) { _ in
       DefaultTabBarCoordinator(navigationController: navigationController)
     }
+
+    container.register(type: HomeCoordinator.self) { _ in
+      DefaultHomeCoordinator(navigationController: navigationController)
+    }
   }
 }
