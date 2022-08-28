b9badd146769aa54c9671113a0e0a1ff049792ef
Iannealer <duwjdtn@ajou.ac.kr>
2022-08-28 23:28:22 +0900

    Feat: #112 - BookmarkCreationReactor & ViewController 임시 생성

---
+++ Tidify/Targets/TidifyPresentation/Sources/BookmarkCreation/Reactor/BookmarkCreationReactor.swift
@@ -0,0 +1,36 @@
+//
+//  BookmarkCreationReactor.swift
+//  TidifyPresentation
+//
+//  Created by Ian on 2022/08/28.
+//  Copyright © 2022 Tidify. All rights reserved.
+//
+
+import ReactorKit
+
+// MARK: - Implementation
+final class BookmarkCreationReactor: Reactor {
+
+  // MARK: - Properties
+  var initialState: State = .init()
+
+  enum Action {
+
+  }
+
+  enum Mutation {
+
+  }
+
+  struct State {
+
+  }
+
+  func mutate(action: Action) -> Observable<Mutation> {
+
+  }
+
+  func reduce(state: State, mutation: Mutation) -> State {
+
+  }
+}
