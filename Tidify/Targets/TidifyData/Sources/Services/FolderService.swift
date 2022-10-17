d416bbcf0874e6a470c12ab1a2d66f161cf0fe19
Hansangjin98 <onetable98@naver.com>
2022-10-18 01:47:45 +0900

    Feat: #127 - Folder Service 구현

---
+++ Tidify/Targets/TidifyData/Sources/Services/FolderService.swift
@@ -0,0 +1,74 @@
+//
+//  FolderService.swift
+//  TidifyDomain
+//
+//  Created by 한상진 on 2022/10/18.
+//  Copyright © 2022 Tidify. All rights reserved.
+//
+
+import Foundation
+import TidifyDomain
+
+import Moya
+
+enum FolderService {
+  case createFolder(_ requestDTO: FolderRequestDTO)
+  case fetchFolders(start: Int = 0, count: Int = 10, keyword: String? = nil)
+  case deleteFolder(id: Int)
+  case updateFolder(id: Int, requestDTO: FolderRequestDTO)
+}
+
+extension FolderService: TargetType {
+  var baseURL: URL {
+    return .init(string: AppProperties.accessToken)!
+  }
+  
+  var path: String {
+    return "/folders"
+  }
+
+  var method: Moya.Method {
+    switch self {
+    case .createFolder:
+      return .post
+    case .fetchFolders:
+      return .get
+    case .updateFolder:
+      return .put
+    case .deleteFolder:
+      return .delete
+    }
+  }
+
+  var task: Task {
+    switch self {
+    case .createFolder(let requestDTO):
+      let param: [String: String] = [
+        "folder_title": requestDTO.title,
+        "folder_color": requestDTO.color
+      ]
+      return .requestParameters(parameters: param, encoding: URLEncoding.queryString)
+      
+    case .fetchFolders:
+      return .requestPlain
+      
+    case .updateFolder(let id, let requestDTO):
+      let param: [String: Any] = [
+        "folder_id": id,
+        "folder_title": requestDTO.title,
+        "folder_color": requestDTO.color
+      ]
+      return .requestParameters(parameters: param, encoding: URLEncoding.queryString)
+      
+    case .deleteFolder(let id):
+      let param: [String: Int] = [
+        "folder_id": id
+      ]
+      return .requestParameters(parameters: param, encoding: URLEncoding.queryString)
+    }
+  }
+
+  var headers: [String : String]? {
+    ["access-token": AppProperties.accessToken]
+  }
+}
