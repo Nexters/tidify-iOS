ec79d125902c2cbfb34a8de6be2a4d2c454c8a51
Iannealer <duwjdtn@ajou.ac.kr>
2022-10-22 23:30:10 +0900

    #133 - TableSectionHeaderView 구현

---
+++ Tidify/Targets/TidifyPresentation/Sources/Setting/VIew/Headers/SettingSectionHeaderView.swift
@@ -0,0 +1,49 @@
+//
+//  SettingSectionHeaderView.swift
+//  TidifyPresentation
+//
+//  Created by Ian on 2022/10/22.
+//  Copyright © 2022 Tidify. All rights reserved.
+//
+
+import UIKit
+
+final class SettingSectionHeaderView: UIView {
+
+  // MARK: - Properties
+  private let titleLabel: UILabel = .init()
+
+  private let section: SettingReactor.Sections
+
+  init(section: SettingReactor.Sections) {
+    self.section = section
+    super.init(frame: .zero)
+
+    setupUI()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+private extension SettingSectionHeaderView {
+  func setupUI() {
+    backgroundColor = .white
+    addSubview(titleLabel)
+
+    if section == .dataManaging {
+      cornerRadius([.topLeft, .topRight], radius: 16)
+    }
+
+    titleLabel.do {
+      $0.text = section.sectionTitle
+      $0.font = .systemFont(ofSize: 16, weight: .bold)
+    }
+
+    titleLabel.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(20)
+      $0.centerY.equalToSuperview()
+    }
+  }
+}
