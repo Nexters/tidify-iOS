4ba117c04fc7aa69680302a26e48e8111c4b1b49
Iannealer <duwjdtn@ajou.ac.kr>
2022-10-26 22:26:40 +0900

    Feat: #133 - SettingScene View 구현

--- Tidify/Targets/TidifyPresentation/Sources/Setting/Reactor/SettingReactor.swift
+++ Tidify/Targets/TidifyPresentation/Sources/Setting/Reactor/SettingReactor.swift
@@ -6,9 +6,38 @@
 //  Copyright © 2022 Tidify. All rights reserved.
 //
 
+import Foundation
+
 import ReactorKit
 
 final class SettingReactor: Reactor {
+
+  enum Sections: Int, CaseIterable {
+    case accountManaging
+    case dataManaging
+
+    var numberOfRows: Int {
+      rowTitles.count
+    }
+
+    var sectionTitle: String {
+      switch self {
+      case .accountManaging:
+        return "계정관리"
+      case .dataManaging:
+        return "데이터 관리"
+      }
+    }
+
+    var rowTitles: [String] {
+      switch self {
+      case .accountManaging:
+        return ["소셜 로그인"]
+      case .dataManaging:
+        return ["이미지 캐시 정리", "모든 캐시 정리", "로그아웃", "앱 버전"]
+      }
+    }
+  }
 
   // MARK: - Properties
   var initialState: State = .init()
@@ -21,22 +50,46 @@ final class SettingReactor: Reactor {
   }
 
   enum Action {
-
+    case didTapCell(indexPath: IndexPath)
   }
 
   enum Mutation {
-
+    case setAlertType(alertType: AlertPresenter.AlertType?)
+    case pushSocialLoginScene
   }
 
   struct State {
-
+    var presentAlert: AlertPresenter.AlertType? = nil
   }
 
   func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case .didTapCell(let indexPath):
+      if indexPath.section == Sections.dataManaging.rawValue {
+        switch indexPath.row {
+        case 0: return .just(.setAlertType(alertType: .removeImageCache))
+        case 1: return .just(.setAlertType(alertType: .removeAllCache))
+        case 2: return .just(.setAlertType(alertType: .logout))
 
+        default:
+          return .just(.setAlertType(alertType: nil))
+        }
+      } else {
+        return .just(.pushSocialLoginScene)
+      }
+    }
   }
 
   func reduce(state: State, mutation: Mutation) -> State {
+    var newState: State = state
 
+    switch mutation {
+    case .pushSocialLoginScene:
+      coordinaotr.pushSocialLoginSettingScene()
+    case .setAlertType(let alertType):
+      newState.presentAlert = alertType
+    }
+
+    return newState
   }
 }
--- Tidify/Targets/TidifyPresentation/Sources/Setting/VIew/SettingViewController.swift
+++ Tidify/Targets/TidifyPresentation/Sources/Setting/VIew/SettingViewController.swift
@@ -10,12 +10,29 @@ import TidifyCore
 import UIKit
 
 import ReactorKit
+import Kingfisher
 
 final class SettingViewController: UIViewController, View {
 
   // MARK: - Properties
+  private let tableView: UITableView = .init(frame: .zero, style: .grouped)
+  private let headerView: SettingTableHeaderView = .init(frame: .init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 80))
+  private let alertPresenter: AlertPresenter
+
+  private let selectedIndexPathSubject: PublishSubject<IndexPath> = .init()
   var disposeBag: DisposeBag = .init()
 
+  // MARK: - Initializer
+  init(alertPresenter: AlertPresenter) {
+    self.alertPresenter = alertPresenter
+
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -30,15 +47,131 @@ final class SettingViewController: UIViewController, View {
 
 // MARK: - Private
 private extension SettingViewController {
+  typealias Action = SettingReactor.Action
+
   func bindAction(reactor: SettingReactor) {
-    // TODO: Implementation
+    selectedIndexPathSubject
+      .asObservable()
+      .map { Action.didTapCell(indexPath: $0) }
+      .bind(to: reactor.action)
+      .disposed(by: disposeBag)
   }
 
   func bindState(reactor: SettingReactor) {
-    // TODO: Implementation
+    reactor.state
+      .map { $0.presentAlert }
+      .asDriver(onErrorDriveWith: .empty())
+      .drive(with: self, onNext: { owner, alertType in
+        guard let alertType = alertType else { return }
+        owner.presentAlert(type: alertType)
+      })
+      .disposed(by: disposeBag)
   }
 
   func setupUI() {
     view.backgroundColor = .white
+    view.addSubview(tableView)
+
+    tableView.do {
+      $0.delegate = self
+      $0.dataSource = self
+      $0.separatorStyle = .singleLine
+      $0.t_registerCellClass(cellType: SettingCell.self)
+      $0.estimatedRowHeight = 75
+      $0.rowHeight = UITableView.automaticDimension
+      $0.tableHeaderView = headerView
+    }
+
+    tableView.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.leading.trailing.bottom.equalToSuperview()
+    }
+  }
+
+  func presentAlert(type: AlertPresenter.AlertType) {
+    var rightButtonAction: ButtonAction? = nil
+
+    if type == .removeAllCache {
+      rightButtonAction = { [weak self] in self?.clearAllCache() }
+    } else if type == .removeImageCache {
+      rightButtonAction = { KingfisherManager.shared.cache.clearMemoryCache() }
+    }
+
+    alertPresenter.present(
+      on: self,
+      alertType: type,
+      leftButtonAction: nil,
+      rightButtonAction: rightButtonAction)
+  }
+
+  func clearAllCache() {
+    let cache = KingfisherManager.shared.cache
+
+    cache.clearCache()
+    cache.cleanExpiredCache()
+  }
+}
+
+// MARK: - UITableViewDataSource
+extension SettingViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return SettingReactor.Sections.allCases.count
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection section: Int
+  ) -> Int {
+    guard let section = SettingReactor.Sections.init(rawValue: section) else { return 0 }
+
+    return section.numberOfRows
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    guard let section = SettingReactor.Sections.init(rawValue: indexPath.section) else { return .init() }
+
+    let cell: SettingCell = tableView.t_dequeueReusableCell(
+      cellType: SettingCell.self,
+      indexPath: indexPath
+    )
+
+    if section.numberOfRows - 1 == indexPath.row {
+      cell.cornerRadius([.bottomLeft, .bottomRight], radius: 16)
+    }
+
+    let rowTitle: String = section.rowTitles[indexPath.row]
+    cell.configure(title: rowTitle)
+
+    return cell
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    viewForHeaderInSection section: Int
+  ) -> UIView? {
+    guard let section = SettingReactor.Sections.init(rawValue: section) else { return nil }
+
+    let sectionHeaderView: SettingSectionHeaderView = .init(section: section)
+    return sectionHeaderView
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    didSelectRowAt indexPath: IndexPath
+  ) {
+    selectedIndexPathSubject.onNext(indexPath)
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension SettingViewController: UITableViewDelegate {
+  func tableView(
+    _ tableView: UITableView,
+    heightForHeaderInSection section: Int
+  ) -> CGFloat {
+    return 75
   }
 }
