510bf5110182a955005194485e413db2002f702e
Hansangjin98 <onetable98@naver.com>
2023-04-26 10:52:57 +0900

    qa: #174 - 온보딩 4번째 페이지 제거

--- Tidify/Targets/TidifyPresentation/Sources/Onboarding/Reactor/OnboardingReactor.swift
+++ Tidify/Targets/TidifyPresentation/Sources/Onboarding/Reactor/OnboardingReactor.swift
@@ -19,7 +19,6 @@ final class OnboardingReactor {
       .init(image: .init(named: "onboardingImage_0")!, buttonTitle: "공유버튼 누르면 바로 북마크 저장"),
       .init(image: .init(named: "onboardingImage_1")!, buttonTitle: "링크주소만 넣어주세요"),
       .init(image: .init(named: "onboardingImage_2")!, buttonTitle: "누구보다 빠르게 기억할게요"),
-      .init(image: .init(named: "onboardingImage_3")!, buttonTitle: "필요한 것만 쏙쏙 공유해주기")
   ], contentIndex: 0)
 
   // MARK: Initializer
