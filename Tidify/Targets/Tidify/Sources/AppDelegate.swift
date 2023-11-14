7ec8203babc2d4ce926d365bfb397f0d3b4357cc
Hansangjin98 <onetable98@naver.com>
2023-11-15 04:07:14 +0900

    feat: #192 - 네비게이션바 구현

--- Tidify/Targets/Tidify/Sources/AppDelegate.swift
+++ Tidify/Targets/Tidify/Sources/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     self.navigationController = navigationController
     window.rootViewController = navigationController
 
+    setupNavigationBar()
     setupLibrary()
     assemble()
 
@@ -50,6 +51,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 }
 
 private extension AppDelegate {
+  func setupNavigationBar() {
+    UIBarButtonItem.appearance().setTitleTextAttributes([.foregroundColor: UIColor.clear], for: .normal)
+    UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.t_gray(weight: 800)]
+    UINavigationBar.appearance().backIndicatorImage = UIImage(named: "backButtonIcon")
+    UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: "backButtonIcon")
+  }
+
   func setupLibrary() {
 
     // KakaoSDK
