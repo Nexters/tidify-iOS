6f3f99f1bd509ad80034fd1b8d144d1c72a802b3
Hansangjin98 <onetable98@naver.com>
2023-11-27 22:16:36 +0900

    feat: #197 - 폴더 생성 뷰 & 뷰모델 구현

---
+++ Tidify/Targets/TidifyPresentation/Sources/Folder/ViewModel/FolderCreationViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  FolderCreationViewModel.swift
+//  TidifyPresentation
+//
+//  Created by 한상진 on 2023/11/22.
+//  Copyright © 2023 Tidify. All rights reserved.
+//
+
+import Combine
+import TidifyDomain
+
+final class FolderCreationViewModel: ViewModelType {
+  typealias UseCase = FolderUseCase
+
+  enum Action {
+    case didTapCreateFolderButton(_ requestDTO: FolderRequestDTO)
+    case didTapUpdateFolderButton(id: Int, requestDTO: FolderRequestDTO)
+  }
+
+  struct State: Equatable {
+    var isLoading: Bool
+    var errorType: FolderError?
+    var isSuccess: Bool
+  }
+
+  let useCase: UseCase
+  @Published var state: State
+
+  init(useCase: UseCase) {
+    self.useCase = useCase
+    state = .init(isLoading: false, errorType: nil, isSuccess: false)
+  }
+
+  func action(_ action: Action) {
+    switch action {
+    case .didTapCreateFolderButton(let requestDTO):
+      createFolder(requestDTO)
+    case .didTapUpdateFolderButton(let id, let requestDTO):
+      updateFolder(id: id, requestDTO: requestDTO)
+    }
+  }
+}
+
+private extension FolderCreationViewModel {
+  func createFolder(_ requestDTO: FolderRequestDTO) {
+    Task {
+      do {
+        state.isLoading = true
+        try await useCase.createFolder(request: requestDTO)
+        state.isLoading = false
+        state.isSuccess = true
+      } catch {
+        state.errorType = .failCreateFolder
+      }
+    }
+  }
+
+  func updateFolder(id: Int, requestDTO: FolderRequestDTO) {
+    Task {
+      do {
+        state.isLoading = true
+        try await useCase.updateFolder(id: id, request: requestDTO)
+        state.isLoading = false
+        state.isSuccess = true
+      } catch {
+        state.errorType = .failUpdateFolder
+      }
+    }
+  }
+}
