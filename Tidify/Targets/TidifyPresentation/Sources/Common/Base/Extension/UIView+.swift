7c29024899bf6ab08366ad40acc162d7ca3b478b
Jeongsu Yeo <duwjdtn1@gmail.com>
2023-12-09 14:52:34 +0900

    feat: UIView 익스텐션으로 TapPublisher 제공

--- Tidify/Targets/TidifyPresentation/Sources/Common/Base/Extension/UIView+.swift
+++ Tidify/Targets/TidifyPresentation/Sources/Common/Base/Extension/UIView+.swift
@@ -6,9 +6,17 @@
 //  Copyright © 2022 Tidify. All rights reserved.
 //
 
+import Combine
 import UIKit
 
 public extension UIView {
+  var tapPublisher: AnyPublisher<Void, Never> {
+    TapPublisher(view: self)
+      .subscribe(on: DispatchQueue.main)
+      .map { _ in }
+      .eraseToAnyPublisher()
+  }
+
   func addTap() -> UITapGestureRecognizer {
     let tapGestureRecognizer: UITapGestureRecognizer = .init()
     addGestureRecognizer(tapGestureRecognizer)
@@ -40,6 +48,62 @@ public extension UIView {
       }
 
       layer.maskedCorners = cornerMask
+    }
+  }
+}
+
+fileprivate extension UIView {
+  struct TapPublisher: Publisher {
+    typealias Output = UITapGestureRecognizer
+    typealias Failure = Never
+
+    let view: UIView
+
+    func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+      let tapGesture = TapGestureRecognizer()
+      view.addGestureRecognizer(tapGesture)
+      let subscription = TapGestureSubscription(subscriber: subscriber, gesture: tapGesture)
+      subscriber.receive(subscription: subscription)
+    }
+  }
+
+  final class TapGestureSubscription<S: Subscriber>: Subscription where S.Input == UITapGestureRecognizer, S.Failure == Never {
+
+    // MARK: Properties
+    private var subscriber: S?
+    private var gesture: TapGestureRecognizer?
+    private var cancellables: Set<AnyCancellable> = .init()
+
+    // MARK: Initializer
+    init(subscriber: S, gesture: TapGestureRecognizer) {
+      self.subscriber = subscriber
+      self.gesture = gesture
+
+      gesture.tappedSubject.sink { [weak self] tap in
+        _ = self?.subscriber?.receive(tap)
+      }
+      .store(in: &cancellables)
+    }
+
+    // MARK: Methods
+    func request(_ demand: Subscribers.Demand) {}
+
+    func cancel() {
+      subscriber = nil
+      gesture = nil
+    }
+  }
+
+  final class TapGestureRecognizer: UITapGestureRecognizer {
+    let tappedSubject = PassthroughSubject<UITapGestureRecognizer, Never>()
+
+    override init(target: Any?, action: Selector?) {
+      super.init(target: target, action: action)
+      addTarget(self, action: #selector(tap))
+    }
+
+    @objc private func tap(sender: UITapGestureRecognizer) {
+      tappedSubject.send(sender)
     }
   }
 }
